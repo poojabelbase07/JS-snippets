b193da75681727650e6e58e7e5ca3816385b79f3
pooja belbase <115167026+poojabelbase07@users.noreply.github.com>
2025-09-18 23:42:09 +0530

    Closure snippets
    A closure is when a function remembers and uses variables from its outer scope, even after that outer function has finished running.

---
+++ closures.js
@@ -0,0 +1,24 @@
+/* Q.1 Write a function expect that helps developers test their code. It should take in any value val and return an object with the following two functions.
+toBe(val) accepts another value and returns true if the two values === each other. If they are not equal, it should throw an error "Not Equal".
+notToBe(val) accepts another value and returns true if the two values !== each other. If they are equal, it should throw an error "Equal". */
+
+// Solution:
+
+var expect = function(val) {
+  return {
+    toBe: function(otherVal) {
+      if (val === otherVal) {
+        return true;
+      } else {
+        throw new Error("Not Equal");
+      }
+    },
+    notToBe: function(otherVal) {
+      if (val !== otherVal) {
+        return true;
+      } else {
+        throw new Error("Equal");
+      }
+    }
+  };
+};
